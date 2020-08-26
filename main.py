0c17d715943b30e73cd2af949943d609fa83e225
suraj@raavana <ptksuraj99@gmail.com>
2020-08-26 15:49:51 +0530

    Video class

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Api, Resource
 
 app = Flask(__name__)
 api = Api(app)
 
-names = {"suraj": {"age": 20, "gender": "male"},
-         "lucy": {"age": 32, "gender": "female"}}
+videos = {}
 
 
-class HelloWorld(Resource):
-    def get(self, name):
-        return names[name]
+class Video(Resource):
+    def get(self, video_id):
+        return videos[video_id]
 
-    def post(self):
-        return {"data": "Posted"}
+    def put(self, video_id):
+        print(request.form)
+        return {}
 
 
-api.add_resource(HelloWorld, '/helloworld/<string:name>')
+api.add_resource(Video, '/video/<int:video_id>')
 
 
 if __name__ == '__main__':
